1df778d0120745be1d23731504d6a7970c3bd1e5
JorshuaEs <estradajorshua16@gmail.com>
2023-01-21 16:32:21 -0600

    first commit

---
+++ src/Gerente.java
@@ -0,0 +1,13 @@
+//					extiende de 
+public class Gerente extends Funcionario{
+	
+	private String clave;
+	
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+	public boolean iniciarSesion(String clave) {
+		return clave == "AluraCursosOnLine";
+	}
+}
